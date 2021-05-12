95156ba9d671de11cfe4417ced29593b788de48d
Jessica Paul <paul.jessica95@gmail.com>
2021-05-12 14:47:02 -0400

    add instructions

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -3,7 +3,8 @@ import './Instructions.css';
 const Instructions = () => {
     return (
         <div className="Instructions">
-            Instructions
+            <h2>Instructions</h2>
+            <p id='instruct-text'>To play, take turns placing X and O in the interior grids. Wherever you play in the interior grid, the next player must make their play in the corresponding exterior grid space. Get three in a row in the interior grid and claim the space on the exterior grid. Get three in a row in the exterior grid and you win the game!</p>
         </div>
     );
 }
