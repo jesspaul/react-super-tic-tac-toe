a4b0901e017efcf1d6fbf47c59b479fee9791dd7
Jessica Paul <paul.jessica95@gmail.com>
2021-05-12 14:56:15 -0400

    add player

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import ExteriorSquare from './components/ExteriorSquare/ExteriorSquare';
 import Gameboard from './components/Gameboard/Gameboard';
 import Instructions from './components/Instructions/Instructions';
+import Player from './components/Player/Player';
 
 function App() {
   return (
     <div className="App">
       <h1>Super Tic Tac Toe</h1>
       <Instructions />
+      <Player />
       <Gameboard />
     </div>
   );
---
+++ src/components/Player/Player.js
@@ -0,0 +1,12 @@
+import './Player.css';
+
+const Player = () => {
+    return (
+        <div className="Player">
+            <h2>Player 1</h2>
+            <p>It's your turn, place your X!</p>
+        </div>
+    );
+}
+ 
+export default Player;
