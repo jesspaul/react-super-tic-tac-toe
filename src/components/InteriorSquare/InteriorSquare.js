eda94716c840711d291156002663ad5ce85bb8bb
Jessica Paul <paul.jessica95@gmail.com>
2021-05-12 16:14:34 -0400

    place symbol on click

--- src/components/InteriorSquare/InteriorSquare.js
+++ src/components/InteriorSquare/InteriorSquare.js
@@ -1,9 +1,18 @@
+import { useState, useContext } from 'react';
+import { GameContext } from '../../contexts/GameContext';
 import './InteriorSquare.css';
 
 const InteriorSquare = () => {
+    const { player } = useContext(GameContext);
+
+    const [value, setValue] = useState(null);
+
+    const handleClick = () => {
+        setValue(player.symbol);
+    }
     return (
-        <div className="InteriorSquare">
-            
+        <div className="InteriorSquare" onClick={handleClick}>
+            {value}
         </div>
     );
 }
